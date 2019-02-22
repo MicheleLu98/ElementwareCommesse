a28a77536b99ce999cb6c021fd2dc7714670ac12
MicheleIlbello <Michele.lunghi@elementware.com>
2019-02-22 16:00:13 +0100

    ciao

--- ElementwareCommesse/Controllers/HomeController.cs
+++ ElementwareCommesse/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ElementwareCommesse.Models;
 
 namespace ElementwareCommesse.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -26,5 +28,38 @@ namespace ElementwareCommesse.Controllers
 
             return View();
         }
-    }
+
+        [HttpGet]
+        public ActionResult ConfiguraSezioni()
+        {
+            DBElementwareCommesseEntities DB = new DBElementwareCommesseEntities();
+            int ID = 0;
+            foreach(var IDSez in DB.TAB_SEZ)
+            {
+                ID = IDSez.IDSez;
+            }
+
+            ID++;
+
+            var SEZ = new TAB_SEZ();
+
+            SEZ.IDSez = ID;
+            
+            return View(SEZ);
+        }
+
+        [HttpPost]
+        public ActionResult ConfiguraSezioni(TAB_SEZ SEZ)
+        {
+            DBElementwareCommesseEntities DB = new DBElementwareCommesseEntities();
+            if (ModelState.IsValid) { 
+
+            DB.TAB_SEZ.Add(SEZ);
+            DB.SaveChanges();
+            }
+
+            return View();
+        }
+
+}
 }
